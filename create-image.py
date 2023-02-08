19ed663085608c7f34a4395ca7a4af8bb21f13d6
wlgns410 <wlgns410@gmail.com>
2023-02-08 13:15:38 +0900

    delete : git cache

--- create-image.py
+++ create-image.py
@@ -38,36 +38,3 @@ for index, image_dict in enumerate(response["data"]):
     image_file = DATA_DIR / f"{file_name.stem}-{index}.png"
     with open(image_file, mode="wb") as png:
         png.write(image_data)
-
-# import openai
-
-
-# YOUR_API_KEY = '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'
-
-
-# def chatGPT(prompt, API_KEY=YOUR_API_KEY):
-    
-#     # set api key
-#     openai.api_key = API_KEY
-
-#     # Call the chat GPT API
-#     completion = openai.Completion.create(
-# 			  engine = 'text-davinci-003'     # 'text-curie-001'  # 'text-babbage-001' #'text-ada-001'
-# 			, prompt = prompt
-# 			, temperature = 0.5 
-# 			, max_tokens = 1024
-# 			, top_p = 1
-# 			, frequency_penalty = 0
-# 			, presence_penalty = 0)
-
-#     return completion['choices'][0]['text']
-
-
-# def main():
-#     # 지문 입력 란
-#     prompt = input("Insert a prompt: ")
-#     print(chatGPT(prompt).strip())
-
-
-# if __name__ == '__main__':
-#     main()
