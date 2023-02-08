a0eae197c9b7991ee347127381640f26ab297394
wlgns410 <wlgns410@gmail.com>
2023-02-08 13:20:15 +0900

    Setting Image Generator

--- create-image.py
+++ create-image.py
@@ -1,30 +1,26 @@
+import os 
 import openai
 import json
 from pathlib import Path
 from base64 import b64decode
-
 from dotenv import load_dotenv
-import os 
+
 
 # load .env
 load_dotenv()
-
 mySecret = os.environ.get('OPENAI_API_KEY')
-
-
-PROMPT = input("Ai가 그려줄 이미지에 대한 묘사: ")
 api_key = mySecret
 
 
+PROMPT = input("Ai가 그려줄 이미지에 대한 묘사: ")
 
-description = 'a flying hamburger in space, digital art'    # Ai가 그려줄 이미지에 대한 묘사
 response = openai.Image.create(prompt=PROMPT            # 텍스트 입력
                                , n=1                    # 생성할 이미지 개수
-                               , size= "1024x1024"      # 이미지 크기
+                               , size= "256×256"        # 이미지 크기(1024×1024 크기 이미지 하나당 2센트, 512×512 크기 이미지는 1.8센트, 256×256 크기 이미지는 1.6센트)
                                , response_format= "b64_json"
-                               , api_key = api_key) # openai API키
+                               , api_key = api_key)     # openai API키
 
- 
+
 DATA_DIR = Path.cwd() # os.getcwd()
 file_name = DATA_DIR / f"{PROMPT[:5]}_{response['created']}.json"  # 생성된 이미지파일 저장 경로
 
